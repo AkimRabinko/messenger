e43369451480cd50e0c1e3daa83798393a7046c6
AkimRabinko <13kj39tg1xr34bd47>
2020-03-22 12:10:20 +0200

    Create immutable logic for argc array creation during encryption

--- src/main/java/com/example/messenger/encryption/aspect/EncryptedAspect.java
+++ src/main/java/com/example/messenger/encryption/aspect/EncryptedAspect.java
@@ -2,24 +2,34 @@ package com.example.messenger.encryption.aspect;
 
 import com.example.messenger.encryption.service.EncryptionService;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class EncryptedAspect {
-    private static final int FIRST_ARGUMENT = 0;
     private final EncryptionService encodingService;
 
     @Around(value = "@annotation(com.example.messenger.encryption.annotation.Encrypted)")
     private Object encryptionValueCall(ProceedingJoinPoint joinPoint) throws Throwable {
         Object[] args = joinPoint.getArgs();
-        args[FIRST_ARGUMENT] = encodingService.encrypt(args[FIRST_ARGUMENT].toString());
-        return joinPoint.proceed(args);
+        return joinPoint.proceed(proceedArgs(args));
+    }
+
+    private Object[] proceedArgs(Object[] args) {
+        Object[] argsParsed = new Object[args.length];
+        for (int i = INTEGER_ZERO; i < args.length; i++) {
+            if (i == INTEGER_ZERO) {
+                argsParsed[i] = encodingService.encrypt(args[i].toString());
+            } else {
+                argsParsed[i] = args[i];
+            }
+        }
+        return argsParsed;
     }
 }
